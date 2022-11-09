15dc756338118f86b8c2a67ef3fc304aa0c5f737
Essen <akitty9499@gmail.com>
2022-11-10 02:00:23 +0800

    update new benchmark source

--- benchmark_source/Mibench/aarch64/automative/susan/runme_large.sh
+++ benchmark_source/Mibench/aarch64/automative/susan/runme_large.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-susan input_large.pgm output_large.smoothing.pgm -s
-susan input_large.pgm output_large.edges.pgm -e
-susan input_large.pgm output_large.corners.pgm -c
+/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.smoothing.pgm -s
+/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.edges.pgm -e
+/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.corners.pgm -c
 
