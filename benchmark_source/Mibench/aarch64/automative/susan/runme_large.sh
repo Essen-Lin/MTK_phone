ce34bd841b9508c26d1f020726ad4bb47107e8c1
Essen <akitty9499@gmail.com>
2022-11-13 05:07:37 +0800

    update source

--- benchmark_source/Mibench/aarch64/automative/susan/runme_large.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.smoothing.pgm -s
-/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.edges.pgm -e
-/data/test/Mibench/susan/susan /data/test/Mibench/susan/input_large.pgm output_large.corners.pgm -c
-
---
+++ benchmark_source/install_benchmark.bat
@@ -0,0 +1,7 @@
+adb push ./dhrystone /data/local/tmp
+adb push ./lmbench-3.0-a9 /data/local/tmp
+adb push ./Mibench /data/local/tmp
+adb push ./speccpu2006 /data/local/tmp
+adb push ./speccpu2017 /data/local/tmp
+
+pause
